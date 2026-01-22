df24cef7c46bc9d59e4dc766c14f067f03afb326
Amyas Merivale <merivale@gmail.com>
2026-01-22 10:13:24 +0100

    Allow zero blocks

--- commands/parseDocument.js
+++ commands/parseDocument.js
@@ -3,6 +3,9 @@ export default (text) => {
   const yamlContent = yamlMatch ? yamlMatch[1] : "";
   const blockContentStart = yamlMatch ? yamlMatch[0].length : 0;
   const blockContent = text.substring(blockContentStart);
-  const blocks = blockContent.replace(/\n{3,}/g, "\n\n").split(/\n\n/);
+  const blocks =
+    blockContent.length > 0
+      ? blockContent.replace(/\n{3,}/g, "\n\n").split(/\n\n/)
+      : [];
   return { yamlContent, blockContent, blocks };
 };
